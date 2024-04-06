ea5b6cfd96aaf7709ed9b57669a6eb3129a9cb3c
NikK173i <160059242+NikK173i@users.noreply.github.com>
2024-04-06 12:23:21 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,41 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2024 NNTU-CS
+#include <iostream>
+#include <string>
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T* ptr;
+    int x;
+public:
+    TStack() : x(0) {
+        ptr = new T[size];
+    }
+    void pushup(const T &c) {
+        if (size - 1 >= x) {
+            ptr[x++] = c;
+        } else {
+            throw std::string("Fall !");
+        }
+    }
+    T popback() {
+        if (x > 0) {
+            return ptr[--x];
+        } else {
+            std::string(" Empty ");
+        }
+    }
+    bool IfZero() const {
+        return x == 0;
+    }
+    T ElemUp() const {
+        if (x > 0) {
+            return ptr[x - 1];
+        } else {
+            throw std::string("Fall!");
+        }
+    }
 };
-
-#endif  // INCLUDE_TSTACK_H_
+#endif  //INCLUDE_TSTACK_H_
